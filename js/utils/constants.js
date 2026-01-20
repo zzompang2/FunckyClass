a79e543a070767cfadc796e4e64d826101663f24
zzompang2 <zzompang2@gmail.com>
2026-01-21 01:44:07 +0900

    strings 삭제, constants로 이동

--- js/utils/constants.js
+++ js/utils/constants.js
@@ -16,5 +16,17 @@
       message:  { label: "문자" },
       consult:  { label: "상담" },
     }),
+
+    BACKUP_FILENAME: "funckyclass",
+
+    DB_MESSAGE: {
+      confirmRestore: "현재 데이터는 모두 덮어씌워집니다. 덮어씌우겠습니까?",
+      successRestore: "데이터를 무사히 불러왔습니다.",
+      confirmDelete: "정말 삭제하시겠습니까?",
+    },
+
+    COLUMNS_WIDTH: {
+      
+    },
   };
 })(window.App);
